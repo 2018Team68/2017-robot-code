90e01e26c54aebb372f7360cf0eb204341516d21
Matt <Matt@DESKTOP-LRH972I>
2017-05-09 20:01:44 -0400

    USBCamera changes for bandwidth

--- src/org/usfirst/frc/team68/robot/subsystems/USBCamera.java
+++ src/org/usfirst/frc/team68/robot/subsystems/USBCamera.java
@@ -2,6 +2,7 @@
 package org.usfirst.frc.team68.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 
@@ -29,12 +30,26 @@ public class USBCamera extends Subsystem {
     
     // Constructor
     private USBCamera() {
+    	int camZeroWidth = 0;
+    	int camZeroHeight = 0;
+    	int camOneWidth = 0;
+    	int camOneHeight = 0;
+    	
+    	SmartDashboard.getNumber("Camera Zero Width", camZeroWidth);
+    	SmartDashboard.getNumber("Camera Zero Height", camZeroHeight);
+    	SmartDashboard.getNumber("Camera One Width", camOneWidth);
+    	SmartDashboard.getNumber("Camera One Height", camOneHeight);
+    	
+    	SmartDashboard.putNumber("Camera Zero Width", camZeroWidth);
+    	SmartDashboard.putNumber("Camera Zero Height", camZeroHeight);
+    	SmartDashboard.putNumber("Camera One Width", camOneWidth);
+    	SmartDashboard.putNumber("Camera One Height", camOneHeight);
+    	
     	camZero = CameraServer.getInstance().startAutomaticCapture(0);
-        camZero.setResolution(384,216);
+        camZero.setResolution(camZeroWidth,camZeroHeight);
         camZero.setFPS(10);
-
         camOne = CameraServer.getInstance().startAutomaticCapture(1);
-        camOne.setResolution(320,240);
+        camOne.setResolution(camOneWidth,camOneHeight);
         camOne.setFPS(10);
     }
     
